7d94c146f8b9c49c59d1d0d212a586ed34d1413f
naimsheikh8020 <naimsheikh8020@gmail.com>
2025-12-13 00:41:04 +0600

    project initial done

---
+++ Gain-solution-ltd/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-center font-bold text-red-400'>Project initial done</h1>
+    </div>
+  )
+}
+
+export default App
